179a12614a6013176f3cc33c3b55c273f2da2b66
kunal khairnar <79435700+kunalkhairnar1198@users.noreply.github.com>
2023-09-27 18:36:50 +0530

    Update task4.js

--- task4.js
+++ task4.js
@@ -1,16 +1,34 @@
 // //GET ELEMENTTAGNAME //
 
 let list = document.getElementsByTagName('li')
-console.log(list)
+// console.log(list)
 list[0].textContent = 'hello'
 list[0].style.fontWeight = 'bold'
 list[0].style.backgroundColor = 'yellow'
 
 // for (var i = 0; i < list.length; i++) {
-//     // list[i].style.backgroundColor = 'red'
+//     list[i].style.backgroundColor = 'red'
 // }
 
+
+
 // add new list element and edit the element
-list[4].style.fontWeight = 'bold'
-list[4].style.backgroundColor = 'red'
-list[4].textContent = 'this list element is edited'
+// list[4].style.fontWeight = 'bold'
+// list[4].style.backgroundColor = 'red'
+// list[4].textContent = 'this list element is edited'
+
+let lieleclass = document.getElementsByClassName("list-group-item");
+// console.log(lieleclass);
+
+let liid = document.getElementsByTagName('li');
+// console.log(liid);
+
+if (lieleclass.length > 0) {
+    lieleclass[0].textContent = 'edited';
+}
+
+if (liid.length > 0) {
+
+    liid[4].textContent = 'liid edited';
+
+}
